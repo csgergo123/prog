76ff2fdc4233223a7e63df2562e0e9fce4d7312a
Csipkes Gergo <csipkesgeri@hotmail.com>
2018-05-09 14:47:40 +0200

    upload

--- labdapattogas/labdapattogas.cpp
+++ labdapattogas/labdapattogas.cpp
@@ -1,62 +1,65 @@
+#include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
-#include <stdlib.h>
+
 using namespace std;
 
 int i;
-static void gotoxy(int x, int y)
-{
-  for(i=0; i<y; i++) cout << "\n";
-  for(i=0; i<x; i++) cout << " ";
-  cout << "o\n";
+
+static void gotoxy(int x, int y) {
+    for(i = 0; i < y; i++) cout << endl;
+    for(i = 0; i < x; i++) cout << " ";
+    cout << "o" << endl;
 }
 
 
-int main(void)
-{
-  int egyx=1;
-  int egyy=-1;
-  int i;
-  int x=10;
-  int y=20;
-  int ty[23];
-  int tx[80];
+int main(void) {
+    int egyx = 1;
+    int egyy = -1;
+    int i;
+    int x = 10;
+    int y = 20;
+    int ty[23];
+    int tx[80];
 
-  for(i=0; i<23; i++)
-       ty[i]=1;
+    for(i = 0; i < 23; i++) {
+        ty[i] = 1;
+    }
 
-  ty[1]=-1; 
-  ty[22]=-1;
+    ty[1] =- 1; 
+    ty[22] = -1;
 
-  for(i=0; i<79; i++)
-       tx[i]=1;
+    for(i = 0; i < 79; i++) {
+        tx[i] = 1;
+    }
 
-  tx[1]=-1;
-  tx[79]=-1;
-
-
-  for(;;)
-  {
-    for(i=0; i<36; i++)
-       cout << "_";
-
-    ("x=%2d", x);
-    ("y=%2d", y);
-
-    for(i=0; i<=36; i++)
-         cout << "_";
+    tx[1] = -1;
+    tx[79] = -1;
 
 
-    (void)gotoxy(x,y);
+    while(1) {
+        for(i = 0; i < 36; i++)
+           cout << "_";
 
-    x+=egyx;
-    y+=egyy;
+        ("x = %2d", x);
+        ("y = %2d", y);
 
-    egyx*=tx[x];
-    egyy*=ty[y];
+        for(i=0; i<=36; i++) {
+            cout << "_";
+        }
 
-    usleep (100000);
-    system("clear");
-  }
+
+      (void)gotoxy(x,y);
+
+      x += egyx;
+      y += egyy;
+
+      egyx *= tx[x];
+      egyy *= ty[y];
+
+      usleep (10000);
+
+      system("clear");
+    }
 
 }
